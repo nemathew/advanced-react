c0c084499be1d2481a6a1f57c28563a27bc4af70
nicmathews <nicmathews@paypal.com>
2021-05-29 18:50:00 -0700

    useState complete

--- src/tutorial/1-useState/setup/2-useState-basics.js
+++ src/tutorial/1-useState/setup/2-useState-basics.js
@@ -1,7 +1,29 @@
-import React, { useState } from 'react';
+import React, { useState } from 'react'
 
 const UseStateBasics = () => {
-  return <h2>useState basic example</h2>;
-};
+  // console.log(useState('hello world'))
+  // const value = useState(1)[0]
+  // const handler = useState(1)[1]
+  // console.log(value, handler)
 
-export default UseStateBasics;
+  const [text, setText] = useState('Hello World')
+
+  const handleClick = () => {
+    if (text === 'Hello World') {
+      setText('Hello People')
+    } else {
+      setText('Hello World')
+    }
+  }
+
+  return (
+    <React.Fragment>
+      <h1>{text}</h1>
+      <button className='btn' onClick={handleClick}>
+        Change Title
+      </button>
+    </React.Fragment>
+  )
+}
+
+export default UseStateBasics
